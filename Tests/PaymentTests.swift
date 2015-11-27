fe146085e921e61dd4d71a7fbcab2cf44a8e49cc
Ben Riazy <hamon.riazy@judopayments.com>
2015-11-27 14:25:42 +0000

    removed unnecessary parameter

--- Tests/PaymentTests.swift
+++ Tests/PaymentTests.swift
@@ -50,9 +50,6 @@ class PaymentTests: XCTestCase {
 
     func testJudoMakeValidPayment() {
         // Given
-        let resource = NSBundle(forClass: self.dynamicType).pathForResource("200-payment", ofType: "json")
-        print(resource)
-        
         guard let references = Reference(consumerRef: "consumer0053252") else { return }
         let address = Address(line1: "242 Acklam Road", line2: "Westbourne Park", line3: nil, town: "London", postCode: "W10 5JJ")
         let card = Card(number: "4976000000003436", expiryDate: "12/15", cv2: "452", address: address)
